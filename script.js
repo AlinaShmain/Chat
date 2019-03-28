cd6d149b378f3cfc9cc5585eb91897559cf73ae9
ALina Ulyanova <ulyanova.alina1997@gmail.com>
2019-03-28 15:59:54 +0300

    modify

--- script.js
+++ script.js
@@ -12,48 +12,100 @@
 //   window.addEventListener("message", listener);
 // } 
 
-let app = {
-	data: {
-		name: '', 
-		msg: '', 
-	methods: {
-
-	}
-}
-
 const url = 'http://localhost:8080/api/chat/message';
 
-let message = null;
+let app = {
+	// data: {
+		name: '', 
+		msg: '',
+	// }, 
+	// methods: {
+		sendMsg: function() {
+    		return () => {
 
-function sendMsg(){
-	if(this.message){
-		axios.post(url, {
-			user: 'user',
-			text: this.message
-		})
+				if(this.data.msg){
+					axios.post(url, {
+						user: this.data.name,
+						text: this.data.msg
+					})
 
-		if(response.status===200){
-                console.log('success')
-               // console.log('save Successfully'+ data);
-                app.singleMsgs = response.data;
-                app.msgFrom= '';
+					if(response.status===200){
+			            console.log('success');
+			            this.data.msg= '';
+			        }
+				}
+			}
+		},
 
-              // /  app.conID = response.data[0].conversation_id;
-            }
-	}
-}
+		addMessages: function(message){
+			return () => {
+				let messages = document.getElementById("messages");
+
+				let name = document.createElement('h2');
+				let text = document.createElement('p');
+
+				messages.appendChild(name, text);
+			}
+		},
+
+		inputHandler: function(e){
+	        if(e.keyCode===13){
+	            this.sendMsg();
+	        }
+    	},
+
+    	clickHandler: function(){
+    		return () => { arguments[0].addEventListener('click', () => {
+	    			let userName = document.getElementById('name').value;
+					let userMsg = document.getElementById('msg').value;
+					if(userName.trim() && userMsg.trim()){
+						console.log('click');
+						// sendMsg()
+					} else return;
+    			})
+    		}
+    	}
+
+
+	// }
+};
+
+
+// let handle = app.clickHandler.bind(app);
+let elem = document.getElementById('send');
+let func = app.clickHandler(elem);
+func();
+
+// let message = null;
+
+// function sendMsg(){
+// 	if(this.message){
+// 		axios.post(url, {
+// 			user: 'user',
+// 			text: this.message
+// 		})
+
+// 		if(response.status===200){
+//                 console.log('success')
+//                 app.singleMsgs = response.data;
+//                 app.msgFrom= '';
+//             }
+// 	}
+// }
 
 let messages = new Array();
 
 
-axios.get(url)
-  .then(response => {
-  	// let data = JSON.parse(response.data[0]);
-  	messages.push(response.data[0])
-  	console.log(response.data[0]);
-});
-
-
 // axios.get(url)
-//   .then(response => console.log(response));
+//   .then(response => {
+//   	messages.push(response.data[0])
+//   	console.log(response.data[0]);
+// });
+
+
+// let sendBtn = document.getElementById('send');
+// sendBtn.addEventListener('click', function(){
+// 	let app.data.name = ;
+// });
+
 
